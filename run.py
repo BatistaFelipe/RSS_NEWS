e481167c848ffb23df2f55efd8ca5f3bd68096fb
root <root@PC-SUPORTE-4.>
2025-03-26 12:32:12 -0300

    ok

--- run.py
+++ run.py
@@ -9,14 +9,16 @@ def datetime_format(dttime):
 	datetime_obj = datetime.strptime(dttime, date_frame)
 	return datetime_obj.strftime("%d/%m/%Y %H:%M:%S")
 
-def make_markdown(data):
-	text = "# G1 - Rio Grande do Sul\n---\n"
+def make_markdown(url, title):
+	data = feedparser.parse(url)
+	text = "# G1 RSS - %s\n---\n" % title
 	for n in data.entries:
 		published = datetime_format(n.published)
 		text += ('- %s: %s **[<span class="material-symbols-outlined">link</span>](%s)**\n' % (published, n.title, n.link))
 	return text
 
-def make_html(md):
+def make_html(url, title):
+	md = make_markdown(url, title)
 	head = '''
 	<link rel="stylesheet" 
 		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
@@ -30,15 +32,13 @@ def make_html(md):
 	</style>
 	'''
 	body = markdown.markdown(md)
-	return (head + body)
+	html = head + body
+	with open(title+'.html', 'w+') as file:
+		file.write(html)
 
 def main():
-	rs_news = feedparser.parse('https://g1.globo.com/dynamo/rs/rio-grande-do-sul/rss2.xml')
-	md = make_markdown(rs_news)
-	html = make_html(md)
-
-	with open('G1RS.html', 'w+') as file:
-		file.write(html)
+	make_html('https://g1.globo.com/dynamo/rs/rio-grande-do-sul/rss2.xml', 'Rio Grande do Sul')
+	make_html('https://g1.globo.com/dynamo/brasil/rss2.xml', 'Brasil')
 
 if __name__ == '__main__':
 	main()
